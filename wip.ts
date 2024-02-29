456192085b46cd9dbbd5aa43112af841640557b7
Dominic Saadi <dominiceliassaadi@gmail.com>
2024-02-28 16:50:06 -0800

    remove wip.ts file

--- wip.ts
+++
@@ -1,8 +0,0 @@
-import { assertRwfwPathAndSetCwd } from './lib/cwd.js'
-import { mergeReleaseBranch } from './release/lib/release.js'
-
-await assertRwfwPathAndSetCwd()
-await mergeReleaseBranch({
-  releaseBranch: 'release/patch/v7.0.4',
-  remote: 'origin',
-})
